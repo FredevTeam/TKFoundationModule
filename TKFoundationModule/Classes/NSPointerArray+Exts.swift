003bc5f2f3e04ccb2f69bae5c0317d3b7adbeda0
抓猫的鱼 <playtomandjerry@gmail.com>
2019-12-30 23:03:00 +0800

    feat: add nspointerarray

---
+++ TKFoundationModule/Classes/NSPointerArray+Exts.swift
@@ -0,0 +1,43 @@
+//
+//  NSPointerArray+Exts.swift
+//  TKFoundationModule
+//
+//  Created by 聂子 on 2019/12/30.
+//
+
+import Foundation
+
+
+extension NSPointerArray {
+    func addObject(_ object: AnyObject?) {
+        guard let strongObject = object else { return }
+        
+        let pointer = Unmanaged.passUnretained(strongObject).toOpaque()
+        addPointer(pointer)
+    }
+    
+    func insertObject(_ object: AnyObject?, at index: Int) {
+        guard index < count, let strongObject = object else { return }
+        
+        let pointer = Unmanaged.passUnretained(strongObject).toOpaque()
+        insertPointer(pointer, at: index)
+    }
+    
+    func replaceObject(at index: Int, withObject object: AnyObject?) {
+        guard index < count, let strongObject = object else { return }
+        
+        let pointer = Unmanaged.passUnretained(strongObject).toOpaque()
+        replacePointer(at: index, withPointer: pointer)
+    }
+    
+    func object(at index: Int) -> AnyObject? {
+        guard index < count, let pointer = self.pointer(at: index) else { return nil }
+        return Unmanaged<AnyObject>.fromOpaque(pointer).takeUnretainedValue()
+    }
+    
+    func removeObject(at index: Int) {
+        guard index < count else { return }
+        
+        removePointer(at: index)
+    }
+}
